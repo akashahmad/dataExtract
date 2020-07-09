455ee8b6333ec2c3735f41ce79af7f3bc3d97502
akashahmad <engr.akashahmad@gmail.com>
2020-07-09 05:33:45 +0500

    date format changed

--- src/stores/wrd_apis/index.js
+++ src/stores/wrd_apis/index.js
@@ -1,59 +1,61 @@
 import axios from "axios";
 
 const getHeaders = () => ({
-  headers: {
-    "Content-Type": "application/json"
-  }
+    headers: {
+        "Content-Type": "application/json"
+    }
 });
 /*
-export const defaultConnection = () =>
-  axios
-    .get(
-      `https://adaptive-file-extract-api-prod.rcs.rsiapps.com/adaptive/v1/research/filters?`
-    )
-    .then(res => res.data || []);
-*/
+ export const defaultConnection = () =>
+ axios
+ .get(
+ `https://adaptive-file-extract-api-prod.rcs.rsiapps.com/adaptive/v1/research/filters?`
+ )
+ .then(res => res.data || []);
+ */
 export const getFilterOptionsDistrict = () =>
-  axios
-    .get(
-      `https://adaptive-file-extract-api-prod.rcs.rsiapps.com/adaptive/v1/research/filters?level=district`
-    )
-    .then(res => res.data || []);
+    axios
+        .get(
+            `https://adaptive-file-extract-api-prod.rcs.rsiapps.com/adaptive/v1/research/filters?level=district`
+        )
+        .then(res => res.data || []);
 
 export const getFilterOptionsBuilding = ids =>
-  axios
-    .get(
-      `https://adaptive-file-extract-api-prod.rcs.rsiapps.com/adaptive/v1/research/filters?level=building&ids=${ids}`
-    )
-    .then(res => res.data || []);
+    axios
+        .get(
+            `https://adaptive-file-extract-api-prod.rcs.rsiapps.com/adaptive/v1/research/filters?level=building&ids=${ids}`
+        )
+        .then(res => res.data || []);
 
 export const getFilterOptionsGrade = ids =>
-  axios
-    .get(
-      `https://adaptive-file-extract-api-prod.rcs.rsiapps.com/adaptive/v1/research/filters?level=grade&ids=${ids}`
-    )
-    .then(res => res.data || []);
+    axios
+        .get(
+            `https://adaptive-file-extract-api-prod.rcs.rsiapps.com/adaptive/v1/research/filters?level=grade&ids=${ids}`
+        )
+        .then(res => res.data || []);
 
 export const getFilterOptionsEventSecond = (test, newFromDate, newToDate) => {
-  const url = !(newFromDate && newToDate)
-    ? `https://adaptive-file-extract-api-prod.rcs.rsiapps.com/adaptive/v1/research/filters?level=event&test=${test}`
-    : `https://adaptive-file-extract-api-prod.rcs.rsiapps.com/adaptive/v1/research/filters?level=event&test=${test}&fromDate=${newFromDate}&toDate=${newToDate}`;
-  console.log("urt", url);
-  return axios.get(url).then(res => res.data || []);
+    newFromDate = new Date(newFromDate);
+    newToDate = new Date(newToDate);
+    const url = !(newFromDate && newToDate)
+        ? `https://adaptive-file-extract-api-prod.rcs.rsiapps.com/adaptive/v1/research/filters?level=event&test=${test}`
+        : `https://adaptive-file-extract-api-prod.rcs.rsiapps.com/adaptive/v1/research/filters?level=event&test=${test}&fromDate=${newFromDate.toJSON()}&toDate=${newToDate.toJSON()}`;
+    console.log("urt", url);
+    return axios.get(url).then(res => res.data || []);
 };
 
 export const getFilterOptionsClass = ids =>
-  axios
-    .get(
-      `https://adaptive-file-extract-api-prod.rcs.rsiapps.com/adaptive/v1/research/filters?level=class&ids=${ids}`
-    )
-    .then(res => res.data || []);
+    axios
+        .get(
+            `https://adaptive-file-extract-api-prod.rcs.rsiapps.com/adaptive/v1/research/filters?level=class&ids=${ids}`
+        )
+        .then(res => res.data || []);
 
 export const getCSVFile = data =>
-  axios
-    .get(
-      `https://adaptive-file-extract-api-prod.rcs.rsiapps.com/adaptive/v1/research?${data}`
-    )
-    .then(res => {
-      console.log(res);
-    });
+    axios
+        .get(
+            `https://adaptive-file-extract-api-prod.rcs.rsiapps.com/adaptive/v1/research?${data}`
+        )
+        .then(res => {
+            console.log(res);
+        });
