714333c84b95b3bccbcabe59bd650cb5d9da36a5
akashahmad <engr.akashahmad@gmail.com>
2020-07-10 02:53:59 +0500

    new ui added

---
+++ src/container/researchData/index.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+import Header from '../../components/header/header'
+import ResearchData from '../../components/reasearchData/researchData'
+import SubmissionPopup from '../../components/submissionPopup/submissionPopup'
+
+export default () => {
+
+    const [submissionPopup, setSubmissionPopup] = useState(false);
+
+    return (
+        <>
+        <SubmissionPopup setSubmissionPopup={setSubmissionPopup} submissionPopup={submissionPopup}/>
+        <Header />
+        <ResearchData setSubmissionPopup={setSubmissionPopup}/>
+        </>
+    );
+}
--- src/layouts/basic/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components/macro';
-
-export const Wrapper = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-  text-align: center;
-`;
